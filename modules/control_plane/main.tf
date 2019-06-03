4fa06e959cbb7f31dbb5b5cea2f99ae0716b2f15
conzetti <conzetti@gmail.com>
2019-06-03 15:56:42 -0400

    Rename A-Record for control-plane LB
    TL;DR
    =====
    - Let's be explicit. This is the control-plane, so call it _that_

--- modules/control_plane/main.tf
+++ modules/control_plane/main.tf
@@ -7,7 +7,7 @@ locals {
 
 resource "azurerm_dns_a_record" "plane" {
   resource_group_name = var.resource_group_name
-  name                = "ci"
+  name                = "control-plane"
   zone_name           = var.dns_zone_name
   ttl                 = "60"
   records             = [azurerm_public_ip.plane.ip_address]
